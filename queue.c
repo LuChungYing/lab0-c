d3f2ef27bdb4ef25f86f27d6d9ef2cb6183818e3
lu <0931848058johnny@gmail.com>
2020-02-25 19:21:54 +0800

    Complete q_reverse and q_sort
    by three tmp
    by mergesort with slow fast pointer to split the list

--- queue.c
+++ queue.c
@@ -125,7 +125,7 @@ bool q_remove_head(queue_t *q, char *sp, size_t bufsize)
  */
 int q_size(queue_t *q)
 {
-    return q->size;
+    return (q == NULL) ? 0 : q->size;
 }
 
 /*
@@ -137,8 +137,29 @@ int q_size(queue_t *q)
  */
 void q_reverse(queue_t *q)
 {
-    /* TODO: You need to write the code for this function */
-    /* TODO: Remove the above comment when you are about to implement. */
+    if (q == NULL)
+        return;
+    if (q->size <= 1)
+        return;
+
+    list_ele_t *tmp1, *tmp2;
+    tmp2 = MULL;
+    q->tail = q->head;
+    while (q->head) {
+        tmp1 = q->head;
+        q->head = q->head->next;
+        tmp1->next = tmp2;
+        if (q->head == NULL) {
+            q->head = tmp1;
+        }
+        tmp2 = q->head->next;
+        q->head->next = tmp1;
+        tmp1 = q->head;
+        if (tmp2 == NULL)
+            break;
+        q->head = tmp2;
+        tmp2 = tmp1;
+    }
 }
 
 /*
@@ -146,8 +167,50 @@ void q_reverse(queue_t *q)
  * No effect if q is NULL or empty. In addition, if q has only one
  * element, do nothing.
  */
+list_ele_t *merge(list_ele_t *l1, list_ele_t *l2)
+{
+    if (l2 == NULL)
+        return l1;
+    if (l1 == NULL)
+        return l2;
+    list_ele_t *tmp = (list_ele_t *) malloc(sizeof(list_ele_t));
+    list_ele_t *re = tmp;
+    while (l1 != NULL && l2 != NULL) {
+        if (strcmp(l1, l2) < 0) {
+            tmp->next = l1;
+            l1 = l1->next;
+        } else {
+            tmp->next = l2;
+            l2 = l2->next;
+        }
+        tmp = tmp->next;
+    }
+    if (l1)
+        tmp->next = l1;
+    if (l2)
+        tmp->next = l2;
+    list_ele_t *head = re->next;
+    free(re);
+    return head;
+}
+list_ele_t *mergesort(list_ele_t *h)
+{
+    if (h == NULL || h->next == NULL)
+        return h;
+    list_ele_t *fast = h->next;
+    list_ele_t *slow = h;
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    fast = slow->next;
+    slow->next = NULL;
+    list_ele_t *l1 = mergesort(h);
+    list_ele_t *l2 = mergesort(fast);
+
+    return merge(l1, l2);
+}
 void q_sort(queue_t *q)
 {
-    /* TODO: You need to write the code for this function */
-    /* TODO: Remove the above comment when you are about to implement. */
+    mergesort(q->head);
 }
